8452b367fb7a8553a07e9fc37f0b6da062cac638
76yoga <brianterer73@gmail.com>
2024-05-03 13:59:59 +0300

    first commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [transactions, setTransaction] = useState([])
+  const [search, setSearch] = useState("");
+  useEffect(()=> {
+    fetch("http://localhost:3000/transactions")
+    .then(r => r.json())
+    .then((data)=> setTransaction(data))
+  }, [])
+  // console.log(transactions)
+
+  function updatedTransactions(newTransactions){
+    const updatedTransactionsArray = [...transactions, newTransactions]
+    setTransaction(updatedTransactionsArray)
+  }
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search searchTerm={search} setSearch={setSearch}/>
+      <AddTransactionForm newData={updatedTransactions}/>
+      <TransactionsList arrayOfAllTransactions={transactions} transactionsSetter={setTransaction} searchTerm={search}/>
     </div>
   );
 }
